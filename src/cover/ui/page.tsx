d58a6b1bf28e8b5311187f01202349050fb55aa2
Sumin <robolindasoo@gmail.com>
2025-01-25 22:34:52 +0900

    :lipstick: 커버 디자인 변경

--- src/cover/ui/page.tsx
+++ src/cover/ui/page.tsx
@@ -1,15 +1,22 @@
 import {cn} from '../../shared/utils/style.ts';
 import {PageLayout} from '../../shared/ui/page-layout.tsx';
+import Cover from '../../assets/cover.svg?react';
 
 export const CoverPage = () => {
   return (
     <PageLayout id="cover">
-      <div className={cn('flex items-center bg-mystic-fog h-full w-full')}>
-        <div className="flex flex-col justify-between text-start px-8 bg-white shadow-2xl py-12 w-full mt-32">
-          <h1 className="text-6xl font-bold mb-8">2025 Planner</h1>
-          <p className="text-lg font-medium text-slate-600">
-            The best way to predict the future is to invent it
-          </p>
+      <div className="relative h-full w-full">
+        <Cover className="h-full w-full" />
+        <div
+          className={cn(
+            'absolute top-0 bottom-0 left-0 right-0 flex items-center h-full w-full',
+          )}>
+          <div className="flex flex-col justify-between text-start px-8 bg-white shadow-2xl py-12 w-full mt-32">
+            <h1 className="text-6xl font-bold mb-8">2025 Planner</h1>
+            <p className="text-lg font-medium text-slate-600">
+              The best way to predict the future is to invent it
+            </p>
+          </div>
         </div>
       </div>
     </PageLayout>
